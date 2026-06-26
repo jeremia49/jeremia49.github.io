3b0b3f451a51408b969713179a3518d7e794e255
jeremia49 <39850621+jeremia49@users.noreply.github.com>
2026-06-26 13:28:59 +0700

    update achievements and certificates

--- src/data/site.ts
+++ src/data/site.ts
@@ -133,62 +133,208 @@ export const awards = [
     year: '2024',
     title: 'Best Graduate Award',
     org: 'Universitas Negeri Medan (UNIMED)',
+    attachments:[
+      {
+        href : "/media/awards/unimed/wisudawan berprestasi.pdf",
+      },
+    ]
   },
   {
     year: '2023',
     title: 'Graduate with Distinction',
     org: 'Bangkit Academy led by Google, Tokopedia, Gojek & Traveloka',
+    attachments:[
+      {
+        href : "/media/awards/bangkit/BANGKIT.jpg",
+      },
+      {
+        href : "/media/awards/bangkit/BANGKIT.pdf",
+      },
+    ]
   },
   {
     year: '2023',
     title: 'PKM–PI Funding Recipient',
     org: '“Kreasi Teselasi Pemanfaatan Etnomatematika pada Motif Ulos Berbasis Website di Koperasi Argado Bona Ni Pinasa” — Kemendikbud',
+    attachments:[
+      {
+        href : "/media/awards/pkm/pkmpi.pdf",
+      },
+    ],
+    
   },
   {
     year: '2022',
     title: 'Second Place, Programming Competition',
     org: 'DIMAS-TI AMLI',
+    attachments:[
+      {
+        href : "/media/awards/dimasti/dimasti.pdf",
+      },
+    ],
+    
   },
   {
     year: '2021',
     title: 'Finalist, Competitive Programming',
     org: 'GEMASTIK',
+    attachments:[
+      {
+        href : "/media/awards/gemastik/2021_cp.pdf",
+      },
+    ],
+    
   },
   {
     year: '2021',
     title: 'Finalist, Capture The Flag',
     org: 'GEMASTIK',
+    attachments:[
+      {
+        href : "/media/awards/gemastik/2021_ctf.pdf",
+      },
+    ],
   },
   {
     year: '2022',
     title: 'PKM–PM Funding Recipient',
     org: '“Smart AR Mathematics Book berbasis Kearifan Lokal…” — Kemendikbud',
+    attachments:[
+      {
+        href : "/media/awards/pkm/pkmpm1.pdf",
+      },
+    ],
   },
   {
     year: '2021',
     title: 'PKM–PM Funding Recipient',
     org: '“Al-Mate: Model Pembelajaran Matematika di Masa Pandemi Berbasis Virtual Web” — Kemendikbud',
+    attachments:[
+      {
+        href : "/media/awards/pkm/pkmpm2.pdf",
+      },
+    ],
+    
   },
 ];
 
 export const certifications = [
-  { year: '2025', title: 'Uji Kemahiran Bahasa Indonesia (Score 695 — Sangat Unggul)', org: 'Kemendikdasmen' },
-  { year: '2025', title: 'TOEFL ITP (Score 597)', org: 'Colorado Course Bengkulu' },
+  { year: '2025', 
+    title: 'Uji Kemahiran Bahasa Indonesia (Score 695 — Sangat Unggul)', 
+    org: 'Kemendikdasmen',
+    attachments:[
+      {
+        href : "/media/certificates/ukbi/UKBI.jpg",
+      },
+      {
+        href : "/media/certificates/ukbi/UKBI.pdf",
+      },
+    ]
+   },
+  { year: '2025', title: 'TOEFL ITP (Score 597)', org: 'Colorado Course Bengkulu',
+    attachments:[
+      {
+        href : "/media/certificates/toefl/toeflitp_colorado.pdf",
+      },
+    ]
+ },
   { year: '2025', title: 'TOEFL ITP (Score 587)', org: 'Universitas Negeri Medan' },
-  { year: '2024', title: 'Back-End for Beginners with JavaScript', org: 'Dicoding Indonesia' },
-  { year: '2024', title: 'Basic JavaScript Programming', org: 'Dicoding Indonesia' },
-  { year: '2023', title: 'TensorFlow Developer Certificate', org: 'TensorFlow Certificate Program' },
-  { year: '2023', title: 'DeepLearning.AI TensorFlow Developer Specialization', org: 'Coursera' },
-  { year: '2023', title: 'TensorFlow: Data and Deployment Specialization', org: 'Coursera' },
-  { year: '2023', title: 'Machine Learning Specialization', org: 'Coursera' },
-  { year: '2023', title: 'Mathematics for Machine Learning Specialization', org: 'Coursera' },
-  { year: '2023', title: 'Data Visualization Fundamentals', org: 'Dicoding Indonesia' },
-  { year: '2023', title: 'Python Programming Fundamentals', org: 'Dicoding Indonesia' },
-  { year: '2022', title: 'Machine Learning for Beginners', org: 'Dicoding Indonesia' },
-  { year: '2022', title: 'Flutter App Development for Beginners', org: 'Dicoding Indonesia' },
-  { year: '2022', title: 'Dart Programming Fundamentals', org: 'Dicoding Indonesia' },
-  { year: '2021', title: 'Python', org: 'Progate' },
-  { year: '2020', title: 'React', org: 'Progate' },
+  { year: '2024', title: 'Back-End for Beginners with JavaScript', org: 'Dicoding Indonesia',
+    attachments:[
+      {
+        href : "https://www.dicoding.com/certificates/QLZ9VV117X5D",
+      },
+    ],
+   },  
+  { year: '2024', title: 'Basic JavaScript Programming', org: 'Dicoding Indonesia',
+    attachments:[
+      {
+        href : "https://www.dicoding.com/certificates/JMZV436QQXN9",
+      },
+    ],
+   },
+  { year: '2023', title: 'TensorFlow Developer Certificate', org: 'TensorFlow Certificate Program',
+    attachments:[
+      {
+        href : "/media/certificates/tensorflow/Tensorflow.jpg",
+      },
+      {
+        label:"Credential",
+        href : "https://www.credential.net/a2a7fecc-0ed4-4114-b16f-032b75ba5976#acc.vd2Fi4rl",
+      },
+    ]
+  },
+  { year: '2023', title: 'DeepLearning.AI TensorFlow Developer Specialization', org: 'Coursera',
+    attachments:[
+      {
+        href : "https://www.coursera.org/account/accomplishments/specialization/certificate/WFDSZR9A53LK",
+      },
+    ] },
+  { year: '2023', title: 'TensorFlow: Data and Deployment Specialization', org: 'Coursera',
+    attachments:[
+      {
+        href : "https://www.coursera.org/account/accomplishments/specialization/certificate/NWVQXYKZ9DW7",
+      },
+    ] },
+  { year: '2023', title: 'Machine Learning Specialization', org: 'Coursera',
+    attachments:[
+      {
+        href : "https://www.dicoding.com/certificates/4EXG9NWVQZRL",
+      },
+    ] },
+  { year: '2023', title: 'Mathematics for Machine Learning Specialization', org: 'Coursera',
+    attachments:[
+      {
+        href : "https://www.coursera.org/account/accomplishments/specialization/certificate/KK7U8WUFNX55",
+      },
+    ] },
+  { year: '2023', title: 'Data Visualization Fundamentals', org: 'Dicoding Indonesia',
+    attachments:[
+      {
+        href : "https://www.dicoding.com/certificates/MEPJK0YVJX3V",
+      },
+    ] },
+  { year: '2023', title: 'Python Programming Fundamentals', org: 'Dicoding Indonesia',
+    attachments:[
+      {
+        href : "https://www.dicoding.com/certificates/07Z6G6W5RXQR",
+      },
+    ] },
+  { year: '2022', title: 'Machine Learning for Beginners', org: 'Dicoding Indonesia',
+    attachments:[
+      {
+        href : "https://www.dicoding.com/certificates/4EXG9NWVQZRL",
+      },
+    ]
+   },
+  { year: '2022', title: 'Flutter App Development for Beginners', org: 'Dicoding Indonesia',
+    attachments:[
+      {
+        href : "https://www.dicoding.com/certificates/4EXG524Y9XRL",
+      },
+    ]
+   },
+  { year: '2022', title: 'Dart Programming Fundamentals', org: 'Dicoding Indonesia',
+    attachments:[
+      {
+        href : "https://www.dicoding.com/certificates/KEXLG8ROMZG2",
+      },
+    ]
+   },
+  { year: '2021', title: 'Python', org: 'Progate',
+    attachments:[
+      {
+        href : "https://progate.com/course_certificate/995ef148qp9tjd",
+      },
+    ]
+   },
+  { year: '2020', title: 'React', org: 'Progate',
+    attachments:[
+      {
+        href : "https://progate.com/course_certificate/51951c94qfnph7",
+      },
+    ]
+   },
 ];
 
 // Navigation shown in the header
